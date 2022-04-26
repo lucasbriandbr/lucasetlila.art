c73f45cf3791abe511f00622b338ef0b31242fe3
lucas <Lucasbriandbr@gmail.com>
2022-04-26 10:23:11 +0200

    Firebase soon

--- lucasetlila.art/pages/components/ChatBox.jsx
+++ lucasetlila.art/pages/components/ChatBox.jsx
@@ -3,36 +3,52 @@ import { useState, useEffect } from 'react'
 
 export default function ChatBox(props) {
 
-    const [ error, setError ] = useState()
-    const [ success, setSuccess ] = useState()
+    const [ error, setError ] = useState('')
+    const [ success, setSuccess ] = useState('')
     const [ theTimeout, setTheTimeout ] = useState(false)
     const [ message, setMessage ] = useState('')
 
     async function signMessage(message) {
         
-        try {
-
-            const encodedMessage = new TextEncoder().encode(message)
-        
-            const signedMessage = await window.solana.signMessage(encodedMessage, "utf8")
-
-            setSuccess('Your message has been sent successfully, please wait')
-            
-            setTheTimeout(true)
-
-            setError('')
-            
-            let successTimeout = setTimeout(() => {setSuccess(''), setTheTimeout(false)}, 3500)
-
-        } catch(err) {
-            
-            setError('An error has occured, please retry, please wait')
+        if (message==='') {
+                
+            setError("You can't send an empty message, please wait")
             
             setTheTimeout(true)
 
             setSuccess('')
             
             let errorTimeout = setTimeout(() => {setError(''), setTheTimeout(false)}, 3500)
+
+        } else {
+        
+            try {
+    
+                const encodedMessage = new TextEncoder().encode(message)
+            
+                const signedMessage = await window.solana.signMessage(encodedMessage, "utf8")
+    
+                setSuccess('Your message has been sent successfully, please wait')
+                
+                setTheTimeout(true)
+    
+                setError('')
+    
+                document.getElementById('name').value=""
+                
+                let successTimeout = setTimeout(() => {setSuccess(''), setTheTimeout(false)}, 3500)
+    
+            } catch(err) {
+                
+                setError('An error has occured, please retry, please wait')
+                
+                setTheTimeout(true)
+    
+                setSuccess('')
+                
+                let errorTimeout = setTimeout(() => {setError(''), setTheTimeout(false)}, 3500)
+    
+            }
 
         }
         
@@ -42,19 +58,21 @@ export default function ChatBox(props) {
 
         <div className={styles.ChatBox}>
 
-            <div className={styles.render}></div>
+            <div className={styles.render}>
+
+                {error!==''?<div className={styles.errorContainer}><p className={styles.infoError}>{error}</p></div>:''}
+
+                {success!==''?<div className={styles.successContainer}><p className={styles.infoSuccess}>{success}</p></div>:''}
+            
+            </div>
             
             <div className={styles.inputAndSendButton}>
 
                 <input className={styles.messageInput} type="text" id="name" name="name" required placeholder={'Type a message, '+props.name}/>
                 
-                <button className={styles.sendingButton} onClick={() => {setMessage(document.getElementById('name').value), signMessage(document.getElementById('name').value)}} disabled={theTimeout}>Send</button>
+                <button className={ `${styles.sendingButton} ${theTimeout? `${styles.disabled}`:`` }` } onClick={() => {setMessage(document.getElementById('name').value), signMessage(document.getElementById('name').value)}} disabled={theTimeout}>Send</button>
 
             </div>
-
-            {/* {error!==''?<div><p className={styles.infoError}>{error}</p></div>:''}
-
-            {success!==''?<div><p className={styles.infoSuccess}>{success}</p></div>:''} */}
         
         </div>
         

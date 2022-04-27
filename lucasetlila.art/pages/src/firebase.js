6139b71909d7913d21c70f7f8a28c797465abb30
lucas <Lucasbriandbr@gmail.com>
2022-04-27 14:14:15 +0200

    Update firebase.js

--- lucasetlila.art/pages/src/firebase.js
+++ lucasetlila.art/pages/src/firebase.js
@@ -13,4 +13,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
-export default {db}
+export {db}
+
+export default function caca() {
+    return 'caca'
+}

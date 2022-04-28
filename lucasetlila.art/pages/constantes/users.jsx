fa86558082f3622052f35b7578650b289046b7f4
lucas <Lucasbriandbr@gmail.com>
2022-04-28 19:19:41 +0200

    Update users.jsx

--- lucasetlila.art/pages/constantes/users.jsx
+++ lucasetlila.art/pages/constantes/users.jsx
@@ -1,6 +1,7 @@
 export const users = [
     { adress:"BKjVzuEvyhaua4Ztby8GPN2NfKhmn7mAasqg8c2J4xmV", state:1, name:"Lila" },
     { adress:"AMmQ38Nqy2yUtsXdYhm4aDvpoH9n9s51HoaC9Fc7meLU", state:2, name:"Lucas" },
+    { adress:"3KdcoyrqRyEdx3LUqfSky4LtkNgMo682LR87Wt8PkfAC", state:3, name:"Lila" },
 ]
 
 export default function numberOfUsers() {
